1a7bbf2d58b26b12176aa1370dd219bff3f724dd
robsetliff <john@claimacademystl.com>
2015-04-29 19:05:42 -0500

    Adding the rest

---
+++ Control Statements/Program.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Control_Statements
+{
+    class IfSelectSwitch
+    {
+        public static void Main()
+        {
+
+            //if-then-else also short hand if then
+            string myInput;
+            int myInt;
+
+            Console.Write("Please enter a number: ");
+            myInput = Console.ReadLine();
+            myInt = Int32.Parse(myInput);
+
+            // Single Decision and Action with braces
+            if (myInt > 0)
+            {
+                Console.WriteLine("Your number {0} is greater than zero.", myInt);
+            }
+
+            // Single Decision and Action without brackets
+            if (myInt < 0)
+                Console.WriteLine("Your number {0} is less than zero.", myInt);
+
+            // Either/Or Decision
+            if (myInt != 0)
+            {
+                Console.WriteLine("Your number {0} is not equal to zero.", myInt);
+            }
+            else
+            {
+                Console.WriteLine("Your number {0} is equal to zero.", myInt);
+            }
+
+            // Multiple Case Decision
+            if (myInt < 0 || myInt == 0)
+            {
+                Console.WriteLine("Your number {0} is less than or equal to zero.", myInt);
+            }
+            else if (myInt > 0 && myInt <= 10)
+            {
+                Console.WriteLine("Your number {0} is in the range from 1 to 10.", myInt);
+            }
+            else if (myInt > 10 && myInt <= 20)
+            {
+                Console.WriteLine("Your number {0} is in the range from 11 to 20.", myInt);
+            }
+            else if (myInt > 20 && myInt <= 30)
+            {
+                Console.WriteLine("Your number {0} is in the range from 21 to 30.", myInt);
+            }
+            else
+            {
+                Console.WriteLine("Your number {0} is greater than 30.", myInt);
+            }
+
+            Console.ReadLine();
+            Console.Clear();
+
+            //another way to do if-then is a single line
+            //(a == 1) ? 20 : 30;
+            //this translates to:
+            //if (a==1) { do this if condition is true} else {do this if condition is false};
+            
+            
+            
+            //goto and switch
+        begin:
+
+            Console.Write("Please enter a number between 1 and 3: ");
+            myInput = Console.ReadLine();
+            myInt = Int32.Parse(myInput);
+             // switch with integer type
+        switch (myInt)
+        {
+            case 1:
+                Console.WriteLine("Your number is {0}.", myInt);
+                break;
+            case 2:
+                Console.WriteLine("Your number is {0}.", myInt);
+                break;
+            case 3:
+                Console.WriteLine("Your number is {0}.", myInt);
+                break;
+            default:
+                Console.WriteLine("Your number {0} is not between 1 and 3.", myInt);
+                break;
+        }
+
+        decide:
+
+        Console.Write("Type \"continue\" to go on or \"quit\" to stop: ");
+        myInput = Console.ReadLine();
+
+        // switch with string type
+        switch (myInput)
+        {
+            case "continue":
+                goto begin;
+            case "quit":
+                Console.WriteLine("Bye.");
+                break;
+            default:
+                Console.WriteLine("Your input {0} is incorrect.", myInput);
+                goto decide;
+        }
+    
+        }
+    }
+
+}
+
---
+++ Loops/Loops/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //while loop
+            int myInt = 0;
+            
+            while (myInt < 10)
+            {
+                    
+                Console.Write("{0} ", myInt);
+                myInt++;
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.Clear();
+            //for loop
+            for (int i = 0; i < 20; i++)
+            {
+                if (i == 10)
+                    break;
+
+                if (i % 2 == 0)
+                    continue;
+
+                Console.Write("{0} ", i);
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.Clear();
+
+
+            //For Each
+            string[] names = { "Cheryl", "Joe", "Matt", "Robert" };
+
+            foreach (string person in names)
+            {
+                Console.WriteLine("{0} ", person);
+            }
+            Console.ReadLine();
+            Console.Clear();
+
+            //do loop
+            string myChoice;
+
+            do
+            {
+                // Print A Menu
+                Console.WriteLine("My Address Book\n");
+
+                Console.WriteLine("A - Add New Address");
+                Console.WriteLine("D - Delete Address");
+                Console.WriteLine("M - Modify Address");
+                Console.WriteLine("V - View Addresses");
+                Console.WriteLine("Q - Quit\n");
+
+                Console.WriteLine("Choice (A,D,M,V,or Q): ");
+
+                // Retrieve the user's choice
+                myChoice = Console.ReadLine();
+
+                // Make a decision based on the user's choice
+                switch (myChoice)
+                {
+                    case "A":
+                    case "a":
+                        Console.WriteLine("You wish to add an address.");
+                        break;
+                    case "D":
+                    case "d":
+                        Console.WriteLine("You wish to delete an address.");
+                        break;
+                    case "M":
+                    case "m":
+                        Console.WriteLine("You wish to modify an address.");
+                        break;
+                    case "V":
+                    case "v":
+                        Console.WriteLine("You wish to view the address list.");
+                        break;
+                    case "Q":
+                    case "q":
+                        Console.WriteLine("Bye.");
+                        break;
+                    default:
+                        Console.WriteLine("{0} is not a valid choice", myChoice);
+                        break;
+                }
+
+                // Pause to allow the user to see the results
+                Console.Write("press Enter key to continue...");
+                Console.ReadLine();
+                Console.WriteLine();
+            } while (myChoice != "Q" && myChoice != "q"); // Keep going until the user wants to quit
+        }
+    }
+}
